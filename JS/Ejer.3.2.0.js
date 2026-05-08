ce83de14b7bc6bb4c39a73e565d024ea759eeca6
Angel <arcangelan2020@gmail.com>
2026-05-08 17:45:09 -0500

    08/05/2026

--- JS/Ejer.3.2.0.js
+++ JS/Ejer.3.2.0.js
@@ -1,2 +1,11 @@
-console.log (hello)
-"LOOK"
+/*PRIMERO*/
+n=16
+
+if (n<0)
+    console.log("El numero es negativo")
+else if ( n<= 10)
+    console.log("El numero es entre 0 y 10")
+else if(n<=20)
+    console.log("El numero es entre 10 y 20")
+else 
+    console.log("El numero es mayor")
---
+++ Python/Eje.3.2.1.py
@@ -0,0 +1,4 @@
+nombre=input("Ingrese su nombre:",)
+can_est=int(input("Cuantos estudiantes va a registrar:",))
+can_notas=int(input("cantidad de notas de cada estudiante:",))
+
