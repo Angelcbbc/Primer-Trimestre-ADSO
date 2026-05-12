5b3c43ed2dfafe56c466915e2676a3851b601fd5
Angel <arcangelan2020@gmail.com>
2026-05-12 15:32:53 -0500

    12/05/2026

--- Python/Eje.3.2.1.py
+++ Python/Eje.3.2.1.py
@@ -1,4 +1,47 @@
-nombre=input("Ingrese su nombre:",)
-can_est=int(input("Cuantos estudiantes va a registrar:",))
-can_notas=int(input("cantidad de notas de cada estudiante:",))
+estudiantes = []
 
+nombre_t = input("Ingrese su nombre: ")
+
+print("Hola", nombre_t)
+
+can_est = int(input("Cuántos estudiantes va a registrar: "))
+
+can_notas = int(input("Cantidad de notas de cada estudiante: "))
+
+for i in range(can_est):
+
+    est_nom = input("Ingrese nombre: ")
+
+    notas = []
+
+    for j in range(can_notas):
+
+        nota = float(input("Ingrese la nota: "))
+
+        notas.append(nota)
+
+    promedio = sum(notas) / len(notas)
+
+    estudiante = {
+        "Nombre": est_nom,
+        "Promedio": promedio
+    }
+
+    estudiantes.append(estudiante)
+
+estudiantes.sort(
+    key=lambda x: x["Promedio"],
+    reverse=True
+)
+
+
+print("\nLISTA FINAL")
+
+
+for estudiante in estudiantes:
+
+    print(
+        estudiante["Nombre"],
+        "---",
+        estudiante["Promedio"]
+    )
