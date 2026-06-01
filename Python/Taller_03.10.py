123b1dd0d8dc97b98f9e6f24a7c01784510881ee
Angel <arcangelan2020@gmail.com>
2026-06-01 17:21:16 -0500

    01/06/2026.100

---
+++ Python/Taller_03.10.py
@@ -0,0 +1,27 @@
+mes=int(input("Ingrese el numero del mes y descubramos su estacion: ",))
+if mes==1:
+    print("Invierno:Diciembre/Enero/Febrero.")
+elif mes==2:
+    print("Invierno:Diciembre/Enero/Febrero.")
+elif mes==12:
+    print("Invierno:Diciembre/Enero/Febrero.")
+elif mes==3:
+    print("Primavera:Marzo/Abril/Mayo.")
+elif mes==4:
+    print("Primavera:Marzo/Abril/Mayo.")
+elif mes==5:
+    print("Primavera:Marzo/Abril/Mayo.")
+elif mes==6:
+    print("Verano:Junio/Julio/Agosto.")
+elif mes==7:
+    print("Verano:Junio/Julio/Agosto.")
+elif mes==8:
+    print("Verano:Junio/Julio/Agosto.")
+elif mes==9:
+    print("Otoño:Septiembre/Octubre/Noviembre.")
+elif mes==10:
+    print("Otoño:Septiembre/Octubre/Noviembre.")
+elif mes==11:
+    print("Otoño:Septiembre/Octubre/Noviembre.")
+else:
+    print("ERROR NO EXISTE")
