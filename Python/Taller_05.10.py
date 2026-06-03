bb097ad7315b98568efabf3859261d5a6618aa08
Tu Nombre <angel@Lenovo-Arcan.>
2026-06-03 00:39:11 -0500

    03/06/2026*2

---
+++ Python/Taller_05.10.py
@@ -0,0 +1,3 @@
+numeros = [10, 20, 10, 30, 20, 40]
+sin_duplicados = list(set(numeros))
+print(sin_duplicados)
---
+++ Python/Taller_05.3.py
@@ -0,0 +1,3 @@
+numeros = [10, 50, 30, 20]
+mayor = max(numeros)
+print("Mayor:", mayor)
